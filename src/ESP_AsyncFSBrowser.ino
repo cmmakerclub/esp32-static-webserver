925e13f803db43c26ec45885764e56a9424f1b91
bavensky <apiruk_sang-ngenchai@hotmail.com>
2019-05-08 23:50:12 +0700

    edit get mac address

--- src/ESP_AsyncFSBrowser.ino
+++ src/ESP_AsyncFSBrowser.ino
@@ -11,38 +11,57 @@
 #include "SPIFFS.h"
 #include <SPIFFSEditor.h>
 
+// add new
+#include "esp_system.h"
+
 // SKETCH BEGIN
 AsyncWebServer server(80);
 AsyncWebSocket ws("/ws");
 AsyncEventSource events("/events");
 
-void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len) {
-  if (type == WS_EVT_CONNECT) {
+void onWsEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len)
+{
+  if (type == WS_EVT_CONNECT)
+  {
     Serial.printf("ws[%s][%u] connect\n", server->url(), client->id());
     client->printf("Hello Client %u :)", client->id());
     client->ping();
-  } else if (type == WS_EVT_DISCONNECT) {
+  }
+  else if (type == WS_EVT_DISCONNECT)
+  {
     Serial.printf("ws[%s][%u] disconnect: %u\n", server->url(), client->id());
-  } else if (type == WS_EVT_ERROR) {
-    Serial.printf("ws[%s][%u] error(%u): %s\n", server->url(), client->id(), *((uint16_t*)arg), (char*)data);
-  } else if (type == WS_EVT_PONG) {
-    Serial.printf("ws[%s][%u] pong[%u]: %s\n", server->url(), client->id(), len, (len) ? (char*)data : "");
-  } else if (type == WS_EVT_DATA) {
-    AwsFrameInfo * info = (AwsFrameInfo*)arg;
+  }
+  else if (type == WS_EVT_ERROR)
+  {
+    Serial.printf("ws[%s][%u] error(%u): %s\n", server->url(), client->id(), *((uint16_t *)arg), (char *)data);
+  }
+  else if (type == WS_EVT_PONG)
+  {
+    Serial.printf("ws[%s][%u] pong[%u]: %s\n", server->url(), client->id(), len, (len) ? (char *)data : "");
+  }
+  else if (type == WS_EVT_DATA)
+  {
+    AwsFrameInfo *info = (AwsFrameInfo *)arg;
     String msg = "";
-    if (info->final && info->index == 0 && info->len == len) {
+    if (info->final && info->index == 0 && info->len == len)
+    {
       //the whole message is in a single frame and we got all of it's data
       Serial.printf("ws[%s][%u] %s-message[%llu]: ", server->url(), client->id(), (info->opcode == WS_TEXT) ? "text" : "binary", info->len);
 
-      if (info->opcode == WS_TEXT) {
-        for (size_t i = 0; i < info->len; i++) {
-          msg += (char) data[i];
+      if (info->opcode == WS_TEXT)
+      {
+        for (size_t i = 0; i < info->len; i++)
+        {
+          msg += (char)data[i];
         }
-      } else {
+      }
+      else
+      {
         char buff[3];
-        for (size_t i = 0; i < info->len; i++) {
-          sprintf(buff, "%02x ", (uint8_t) data[i]);
-          msg += buff ;
+        for (size_t i = 0; i < info->len; i++)
+        {
+          sprintf(buff, "%02x ", (uint8_t)data[i]);
+          msg += buff;
         }
       }
       Serial.printf("%s\n", msg.c_str());
@@ -51,9 +70,12 @@ void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventT
         client->text("I got your text message");
       else
         client->binary("I got your binary message");
-    } else {
+    }
+    else
+    {
       //message is comprised of multiple frames or the frame is split into multiple packets
-      if (info->index == 0) {
+      if (info->index == 0)
+      {
         if (info->num == 0)
           Serial.printf("ws[%s][%u] %s-message start\n", server->url(), client->id(), (info->message_opcode == WS_TEXT) ? "text" : "binary");
         Serial.printf("ws[%s][%u] frame[%u] start[%llu]\n", server->url(), client->id(), info->num, info->len);
@@ -61,22 +83,29 @@ void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventT
 
       Serial.printf("ws[%s][%u] frame[%u] %s[%llu - %llu]: ", server->url(), client->id(), info->num, (info->message_opcode == WS_TEXT) ? "text" : "binary", info->index, info->index + len);
 
-      if (info->opcode == WS_TEXT) {
-        for (size_t i = 0; i < info->len; i++) {
-          msg += (char) data[i];
+      if (info->opcode == WS_TEXT)
+      {
+        for (size_t i = 0; i < info->len; i++)
+        {
+          msg += (char)data[i];
         }
-      } else {
+      }
+      else
+      {
         char buff[3];
-        for (size_t i = 0; i < info->len; i++) {
-          sprintf(buff, "%02x ", (uint8_t) data[i]);
-          msg += buff ;
+        for (size_t i = 0; i < info->len; i++)
+        {
+          sprintf(buff, "%02x ", (uint8_t)data[i]);
+          msg += buff;
         }
       }
       Serial.printf("%s\n", msg.c_str());
 
-      if ((info->index + len) == info->len) {
+      if ((info->index + len) == info->len)
+      {
         Serial.printf("ws[%s][%u] frame[%u] end[%llu]\n", server->url(), client->id(), info->num, info->len);
-        if (info->final) {
+        if (info->final)
+        {
           Serial.printf("ws[%s][%u] %s-message end\n", server->url(), client->id(), (info->message_opcode == WS_TEXT) ? "text" : "binary");
           if (info->message_opcode == WS_TEXT)
             client->text("I got your text message");
@@ -88,28 +117,32 @@ void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventT
   }
 }
 
-
-const char* ssid = "CMMC_Sinet_2.4G";
-const char* password = "zxc12345";
-const char * hostName = "2600:";
-const char* http_username = "admin";
-const char* http_password = "admin";
+const char *ssid = "CMMC_Sinet_2.4G";
+const char *password = "zxc12345";
+const char *hostName = "2600:";
+const char *http_username = "admin";
+const char *http_password = "admin";
 
 static char sta_mac[18];
 static char softap_mac[18];
 
-void scanNetworks() {
+void scanNetworks()
+{
   Serial.println("scan start");
 
   // WiFi.scanNetworks will return the number of networks found
   int n = WiFi.scanNetworks();
   Serial.println("scan done");
-  if (n == 0) {
+  if (n == 0)
+  {
     Serial.println("no networks found");
-  } else {
+  }
+  else
+  {
     Serial.print(n);
     Serial.println(" networks found");
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; ++i)
+    {
       // Print SSID and RSSI for each network found
       Serial.print(i + 1);
       Serial.print(": ");
@@ -118,24 +151,74 @@ void scanNetworks() {
       Serial.print(WiFi.RSSI(i));
       Serial.print(")");
       Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN) ? " " : "*");
-      ws.textAll((char*)WiFi.SSID(i).c_str());
+      ws.textAll((char *)WiFi.SSID(i).c_str());
       delay(10);
     }
   }
   Serial.println("");
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
-  uint64_t chipid;
-  chipid = ESP.getEfuseMac();
+  // uint64_t chipid;
+  // chipid = ESP.getEfuseMac();
+  // char strID[16];
+  // sprintf(strID, "%08x", (uint32_t)chipid);
+  // strcpy(sta_mac, "CMMC-");
+  // strcpy(sta_mac + 5, strID);
+  // strcpy(softap_mac, WiFi.softAPmacAddress().c_str());
+  // Serial.setDebugOutput(true);
+
+  // ===================================================
+  // uint8_t mac[6];
+  // char temp[16];
+  // uint8_t chipaddress[16];
+  // String addressSTA;
+  // esp_efuse_mac_get_default(chipaddress);
+  // //esp_read_mac(chipid,0);
+  // sprintf(temp, "%X", (unsigned int)chipaddress);
+  // addressSTA = String(temp);
+  // addressSTA.toLowerCase();
+  // Serial.print("chipaddress: ");
+  // Serial.println(addressSTA);
+
+  // esp_read_mac(mac, ESP_MAC_WIFI_STA);
+  // sprintf(temp, MACSTR, MAC2STR(mac));
+  // Serial.print("wifiaddress: ");
+  // Serial.println(temp);
+
+  String address;
+  uint8_t baseMac[6];
+  esp_read_mac(baseMac, ESP_MAC_WIFI_STA);
+  char baseMacChr[18] = {0};
+  sprintf(baseMacChr, "%02X%02X%02X%02X", baseMac[2], baseMac[3], baseMac[4], baseMac[5]);
+  address = baseMacChr;
+  address.toLowerCase();
+
   char strID[16];
-  sprintf(strID, "%08x", (uint32_t)chipid);
-  strcpy(sta_mac, "CMMC-");
+  sprintf(strID, "%s", address.c_str());
+  strcpy(sta_mac, "2600-");
   strcpy(sta_mac + 5, strID);
-  strcpy(softap_mac, WiFi.softAPmacAddress().c_str());
+  Serial.print("sta_mac: ");
   Serial.println(sta_mac);
+  // strcpy(softap_mac, WiFi.softAPmacAddress().c_str());
   Serial.setDebugOutput(true);
+
+  // String macAddressWIFI;
+  // uint8_t baseMac[6];
+  // uint8_t espMac[16];
+  // // Get MAC address for WiFi station
+  // esp_read_mac(baseMac, ESP_MAC_WIFI_STA);
+  // char baseMacChr[18] = {0};
+  // // sprintf(baseMacChr, "%02X:%02X:%02X:%02X:%02X:%02X", baseMac[0], baseMac[1], baseMac[2], baseMac[3], baseMac[4], baseMac[5]);
+  // sprintf(baseMacChr, "%02X%02X%02X%02X%02X%02X", baseMac[0], baseMac[1], baseMac[2], baseMac[3], baseMac[4], baseMac[5]);
+  // macAddressWIFI = String(baseMacChr);
+  // macAddressWIFI.toLowerCase();
+  // Serial.print("baseMacChr ");
+  // Serial.println(macAddressWIFI);
+  // ===================================================
+
   // WiFi.hostname(hostName);
   scanNetworks();
 
@@ -145,7 +228,8 @@ void setup() {
   WiFi.softAPConfig(Ip, Ip, NMask);
   WiFi.softAP(sta_mac, sta_mac + 5);
   WiFi.begin(ssid, password);
-  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+  if (WiFi.waitForConnectResult() != WL_CONNECTED)
+  {
     Serial.printf("STA: Failed!\n");
     WiFi.disconnect(false);
     delay(1000);
@@ -165,11 +249,16 @@ void setup() {
     events.send(p, "ota");
   });
   ArduinoOTA.onError([](ota_error_t error) {
-    if (error == OTA_AUTH_ERROR) events.send("Auth Failed", "ota");
-    else if (error == OTA_BEGIN_ERROR) events.send("Begin Failed", "ota");
-    else if (error == OTA_CONNECT_ERROR) events.send("Connect Failed", "ota");
-    else if (error == OTA_RECEIVE_ERROR) events.send("Recieve Failed", "ota");
-    else if (error == OTA_END_ERROR) events.send("End Failed", "ota");
+    if (error == OTA_AUTH_ERROR)
+      events.send("Auth Failed", "ota");
+    else if (error == OTA_BEGIN_ERROR)
+      events.send("Begin Failed", "ota");
+    else if (error == OTA_CONNECT_ERROR)
+      events.send("Connect Failed", "ota");
+    else if (error == OTA_RECEIVE_ERROR)
+      events.send("Recieve Failed", "ota");
+    else if (error == OTA_END_ERROR)
+      events.send("End Failed", "ota");
   });
   ArduinoOTA.setHostname(hostName);
   ArduinoOTA.begin();
@@ -181,20 +270,20 @@ void setup() {
   ws.onEvent(onWsEvent);
   server.addHandler(&ws);
 
-  events.onConnect([](AsyncEventSourceClient * client) {
+  events.onConnect([](AsyncEventSourceClient *client) {
     client->send("hello!", NULL, millis(), 1000);
   });
   server.addHandler(&events);
 
   // server.addHandler(new SPIFFSEditor(http_username,http_password));
 
-  server.on("/heap", HTTP_GET, [](AsyncWebServerRequest * request) {
+  server.on("/heap", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send(200, "text/plain", String(ESP.getFreeHeap()));
   });
 
   server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.htm");
 
-  server.onNotFound([](AsyncWebServerRequest * request) {
+  server.onNotFound([](AsyncWebServerRequest *request) {
     Serial.printf("NOT_FOUND: ");
     if (request->method() == HTTP_GET)
       Serial.printf("GET");
@@ -214,53 +303,62 @@ void setup() {
       Serial.printf("UNKNOWN");
     Serial.printf(" http://%s%s\n", request->host().c_str(), request->url().c_str());
 
-    if (request->contentLength()) {
+    if (request->contentLength())
+    {
       Serial.printf("_CONTENT_TYPE: %s\n", request->contentType().c_str());
       Serial.printf("_CONTENT_LENGTH: %u\n", request->contentLength());
     }
 
     int headers = request->headers();
     int i;
-    for (i = 0; i < headers; i++) {
-      AsyncWebHeader* h = request->getHeader(i);
+    for (i = 0; i < headers; i++)
+    {
+      AsyncWebHeader *h = request->getHeader(i);
       Serial.printf("_HEADER[%s]: %s\n", h->name().c_str(), h->value().c_str());
     }
 
     int params = request->params();
-    for (i = 0; i < params; i++) {
-      AsyncWebParameter* p = request->getParam(i);
-      if (p->isFile()) {
+    for (i = 0; i < params; i++)
+    {
+      AsyncWebParameter *p = request->getParam(i);
+      if (p->isFile())
+      {
         Serial.printf("_FILE[%s]: %s, size: %u\n", p->name().c_str(), p->value().c_str(), p->size());
-      } else if (p->isPost()) {
+      }
+      else if (p->isPost())
+      {
         Serial.printf("_POST[%s]: %s\n", p->name().c_str(), p->value().c_str());
-      } else {
+      }
+      else
+      {
         Serial.printf("_GET[%s]: %s\n", p->name().c_str(), p->value().c_str());
       }
     }
 
     request->send(404);
   });
-  server.onFileUpload([](AsyncWebServerRequest * request, const String & filename, size_t index, uint8_t *data, size_t len, bool final) {
+  server.onFileUpload([](AsyncWebServerRequest *request, const String &filename, size_t index, uint8_t *data, size_t len, bool final) {
     if (!index)
       Serial.printf("UploadStart: %s\n", filename.c_str());
-    Serial.printf("%s", (const char*)data);
+    Serial.printf("%s", (const char *)data);
     if (final)
       Serial.printf("UploadEnd: %s (%u)\n", filename.c_str(), index + len);
   });
-  server.onRequestBody([](AsyncWebServerRequest * request, uint8_t *data, size_t len, size_t index, size_t total) {
+  server.onRequestBody([](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total) {
     if (!index)
       Serial.printf("BodyStart: %u\n", total);
-    Serial.printf("%s", (const char*)data);
+    Serial.printf("%s", (const char *)data);
     if (index + len == total)
       Serial.printf("BodyEnd: %u\n", total);
   });
   server.begin();
 }
 
-
-void loop() {
+void loop()
+{
   // ArduinoOTA.handle();
-  if ( (millis() % (20*1000L)) == 0) {
+  if ((millis() % (20 * 1000L)) == 0)
+  {
     // scanNetworks();
     Serial.println("HELLO");
   }
